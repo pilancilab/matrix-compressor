0034f1426340401e139e99fdcdf4251fe7afa409
Varun Srivastava <varunsrivastava.v@gmail.com>
2023-04-24 04:31:05 -0700

    feat: compute svdvals for llama

--- scripts/llama/hf_loader.py
+++ scripts/llama/hf_loader.py
@@ -1,15 +1,27 @@
 from loguru import logger
 
+import torch
+import torch.nn as nn
 from llama_compression import hf_load, setup_model_parallel
 
 
 def main():
+    torch.set_printoptions(precision=3)
     ckpt_dir = "./artifacts/llama-7b-hf"
 
     generator = hf_load(ckpt_dir)
-
+    param_dict = {}
     for name, layer in generator.model.named_parameters():
-        logger.debug(f"Layer: {name} has shape {layer.shape}")
+        param = layer.data.detach()
+        rank = -1
+        if param.ndim == 2:
+            rank = torch.linalg.matrix_rank(param)
+            svd_vals = torch.linalg.svdvals(param)
+            param_dict[name] = svd_vals
+        logger.debug(f"Layer: {name} has shape {layer.shape} with rank {rank}")
+        
+        
+    torch.save(param_dict, "misc/llama/llama_singular_vals.pt")
 
 
 if __name__ == "__main__":
