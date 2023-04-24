89ee0f1038fee395121354e20c8f313ea361b749
Varun Srivastava <varunsrivastava.v@gmail.com>
2023-04-24 04:19:31 -0700

    refactor: increase logging

--- src/lplr/utils.py
+++ src/lplr/utils.py
@@ -1,6 +1,6 @@
 import numpy as np
 import torch
-
+from loguru import logger
 
 def normalize_and_shift_wrt_inner_prod(X: torch.Tensor = None, Y: torch.Tensor = None):
     """
@@ -18,15 +18,19 @@ def normalize_and_shift_wrt_inner_prod(X: torch.Tensor = None, Y: torch.Tensor =
     t1 = torch.linalg.norm(Y, ord="fro") ** 2 - torch.sum(torch.multiply(Y, M)) ** 2 / (
         n * d
     )
+    logger.trace(f"t1 = {t1:.5f}")
     t2 = torch.sum(torch.multiply(X, Y)) - torch.sum(torch.multiply(Y, M)) * torch.sum(
         torch.multiply(X, M)
     ) / (n * d)
+    
+    logger.trace(f"t2 = {t2:.5f}")
     alpha = t2 / t1
-
+    logger.trace(f"alpha = {alpha:.5f}")
     beta = (
         torch.sum(torch.multiply(X, M)) - torch.sum(torch.multiply(Y, M)) * alpha
     ) / (n * d)
-
+    logger.trace(f"beta = {beta:.5f}")
     return alpha * Y + beta * M
+
 
 
